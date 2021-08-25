fd082b299f9d0e3b803a6a799b8c708aa6c2d290
Kehinded <ayenikehinded@gmail.com>
2021-08-25 04:09:44 +0100

    project array

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,6 +9,37 @@ import { Link, useLocation } from 'react-router-dom'
 import { FaBoxes } from 'react-icons/fa'
 // import { useHistory } from 'react-router-dom'
 
+const projects = [
+    {
+        image: `${HomeTeach}`,    
+            text: 'Worked in a team with backend developer and designers to build a  site that help teachers to render their services effectively and also allows student to sign up and choose select courses from the list of curated top performing best reviewed best instructors, built this with hthml, css and javascript.', 
+            title: 'homeTeach',
+            link: 'https://staging-hometeach80.netlify.app/index.html',
+            github: 'https://github.com/zuri-training/HomeTeach-fe-pjt-80.git'
+    },
+    {
+        image: `${hospital_home}`,
+        title: 'Akot Hospital',
+        text : 'An online hospital where patient can book an appointent with a clinical worker and also where medical staffs can access their patients and get daily schedules, built this with hthml, css and javascript.',
+        link : 'https://friendly-einstein-2d6ee0.netlify.app',
+        github : 'https://github.com/Kehinded/AKOT-HOSPITAL.git'
+    },
+    {
+        image: `${AkotCompany}`,
+        text: 'Four in one company comprises of a farm, airport, hotel and fuel station where customers can book for online services of their choice collectively . Built with html, advance css, sass and javascript.',
+        title: 'Akot Companies',
+        link: 'https://kehinded.github.io/My-first-main-demo-site/',
+        github : 'https://github.com/Kehinded/My-first-main-demo-site.git'
+    },
+    {
+        image : `${DiceGame}`,
+            title : 'dice game',
+            text: 'An online dice game with two players racing to set a winning value score to catch fun. Built this with html, bootstrap and javascript.',
+            link : 'https://kehinded.github.io/DICE-GAME/',
+            github : 'https://github.com/Kehinded/DICE-GAME.git'
+    }
+]
+
 const Project = ({projectTitle}) => {
     const location = useLocation()
     // const history = useHistory()
@@ -18,34 +49,25 @@ const Project = ({projectTitle}) => {
     return (
         <div className= {`project-section ${location.pathname === '/project' ? 'real-project' : ''}`}>
             <h2 className="title">{projectTitle}</h2>
-            <ProjectViewLeft 
-            image={HomeTeach}    
-            text= 'Worked in a team with backend developer and designers to build a  site that help teachers to render their services effectively and also allows student to sign up and choose select courses from the list of curated top performing best reviewed best instructors, built this with hthml, css and javascript.' 
-            title = 'homeTeach'
-            link = 'https://staging-hometeach80.netlify.app/index.html'
-            github = 'https://github.com/zuri-training/HomeTeach-fe-pjt-80.git'
-            />
-            <ProjectViewRight image={hospital_home}
-            title= 'Akot Hospital'
-            text = 'An online hospital where patient can book an appointent with a clinical worker and also where medical staffs can access their patients and get daily schedules, built this with hthml, css and javascript.'
-            link = 'https://friendly-einstein-2d6ee0.netlify.app'
-            github = 'https://github.com/Kehinded/AKOT-HOSPITAL.git'
-            />
-            <ProjectViewLeft 
-            image= {AkotCompany}
-            text= 'Four in one company comprises of a farm, airport, hotel and fuel station where customers can book for online services of their choice collectively . Built with html, advance css, sass and javascript.'
-            title= 'Akot Companies'
-            link= 'https://kehinded.github.io/My-first-main-demo-site/'
-            github = 'https://github.com/Kehinded/My-first-main-demo-site.git'
-            />
-            <ProjectViewRight
-            image = {DiceGame}
-            title = 'dice game'
-            text= 'An online dice game with two players racing to set a winning value score to catch fun. Built this with html, bootstrap and javascript.'
-            link = 'https://kehinded.github.io/DICE-GAME/'
-            github = 'https://github.com/Kehinded/DICE-GAME.git'
-            />
-
+         {
+             projects.map((child, index) => (
+                 index % 2 === 0 ? (<ProjectViewLeft 
+                 key = {index}
+                 image = {child.image}
+                 title = {child.title}
+                 text = {child.text}
+                 link = {child.link}
+                 github = {child.github}
+                 />) : (<ProjectViewRight 
+                    key = {index}
+                    image = {child.image}
+                    title = {child.title}
+                    text = {child.text}
+                    link = {child.link}
+                    github = {child.github}
+                 />)
+             ))
+         }
          {location.pathname === '/' &&  <Link to = '/project'  className= 'project-all-btn'> <p>view all projects</p> <FaBoxes /> </Link>}
         </div>
     )
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,11 @@
-import Project from '../components/Project'
+// import Project from '../components/Project'
 import '../styles/Project.css'
 import ProjectViewLeft from '../components/ProjectViewLeft'
 import ProjectViewRight from '../components/ProjectViewRight'
+import HomeTeach from '../images/homeTeach-site.png'
+import hospital_home from '../images/hospital-home-2-site.png'
+import AkotCompany from '../images/akot-farm.png'
+import DiceGame from '../images/dice-game-site.png'
 import NatourImg from '../images/natour-site.png'
 import BudgetyImg from '../images/budgety-app.png'
 import NexterImg from '../images/nexter-home-site.png'
@@ -10,61 +14,111 @@ import MeetUp from '../images/details-react-app.png'
 import PortfolioImg from '../images/portfolio.png'
 import ResumeImg from '../images/resume.png'
 
+const projects = [
+    {
+        image: `${HomeTeach}`,    
+            text: 'Worked in a team with backend developer and designers to build a  site that help teachers to render their services effectively and also allows student to sign up and choose select courses from the list of curated top performing best reviewed best instructors, built this with hthml, css and javascript.', 
+            title: 'homeTeach',
+            link: 'https://staging-hometeach80.netlify.app/index.html',
+            github: 'https://github.com/zuri-training/HomeTeach-fe-pjt-80.git'
+    },
+    {
+        image: `${hospital_home}`,
+        title: 'Akot Hospital',
+        text : 'An online hospital where patient can book an appointent with a clinical worker and also where medical staffs can access their patients and get daily schedules, built this with hthml, css and javascript.',
+        link : 'https://friendly-einstein-2d6ee0.netlify.app',
+        github : 'https://github.com/Kehinded/AKOT-HOSPITAL.git'
+    },
+    {
+        image: `${AkotCompany}`,
+        text: 'Four in one company comprises of a farm, airport, hotel and fuel station where customers can book for online services of their choice collectively . Built with html, advance css, sass and javascript.',
+        title: 'Akot Companies',
+        link: 'https://kehinded.github.io/My-first-main-demo-site/',
+        github : 'https://github.com/Kehinded/My-first-main-demo-site.git'
+    },
+    {
+        image : `${DiceGame}`,
+            title : 'dice game',
+            text: 'An online dice game with two players racing to set a winning value score to catch fun. Built this with html, bootstrap and javascript.',
+            link : 'https://kehinded.github.io/DICE-GAME/',
+            github : 'https://github.com/Kehinded/DICE-GAME.git'
+    },
+    {
+        image : `${NatourImg}`,
+        title : 'natour ',
+        text : "Enjoy your vacation booking an online trip through natour site. Built this with html and css only.",
+        link : 'https://kehinded.github.io/CSS-Nature-only/',
+        github : 'https://github.com/Kehinded/CSS-Nature-only.git'
+    },
+    {
+        image : `${NexterImg}`,
+            title : 'Billionairerlts',
+            text : 'an online real estate website for viewing and selling of house properties, where you can get to meet up with realtors and chose house of your choice. Built with html and advance grids',
+            link : 'https://kehinded.github.io/FIRST-CSS-GRID-SITE/',
+            github : 'https://github.com/Kehinded/FIRST-CSS-GRID-SITE.git'
+    },
+    {
+        image : `${HotelImg}`,
+            title : 'Hotel',
+            text : 'Advance online hotel view site for easy online booking . Built with html and css flexbox.',
+            link : 'https://kehinded.github.io/HOTEL/',
+            github : 'https://github.com/Kehinded/HOTEL.git'
+    },
+    {
+        image : `${BudgetyImg}`,
+            title : 'budget app',
+            text : 'a website where you can calculate income and expenses with their data structures. Built this with html, bootstrap and javascript.',
+            link : 'https://kehinded.github.io/Budget-Calculation-app/',
+            github : 'https://github.com/Kehinded/Budget-Calculation-app.git'
+    },
+    {
+        image : `${MeetUp}`,
+            title : 'Contact save',
+            text : 'online app where you can input, save, remove details or contacts. Built with react .',
+            link : ' https://kehinded.github.io/react-meet-up/',
+            github : 'https://github.com/Kehinded/react-meet-up.git'
+    },
+    {
+        image : `${PortfolioImg}`,
+        title : 'My portfolio',
+        text : 'This current website you are navigating. Built with react and sass with Api actions at the contact and like box, you can get my contacts from the footer.',
+        link : 'https://timi-portfolio.netlify.app/',
+        github : 'https://github.com/Kehinded/Timi-portfolio.git'
+    },
+    {
+        image : `${ResumeImg}`,
+            title : 'My resume',
+            text : 'My online resume. Built with react and sass.',
+            link : 'https://kehinded.github.io/hngi8-task-cv/',
+            github : 'https://github.com/Kehinded/hngi8-task-cv.git'
+    }
+]
+
 const Projects = () => {
     return (
         <div className= 'project-page'>
-            <Project projectTitle = 'all project'/>
+            <h2 className="title">all projects</h2>
             <div className="project-section">
-                <ProjectViewLeft
-                image = {NatourImg}
-                title = 'natour '
-                text = "Enjoy your vacation booking an online trip through natour site. Built this with html and css only."
-                link = 'https://kehinded.github.io/CSS-Nature-only/'
-                github = 'https://github.com/Kehinded/CSS-Nature-only.git'
-            />
-                    <ProjectViewRight 
-            image = {NexterImg}
-            title = 'Billionairerlts'
-            text = 'an online real estate website for viewing and selling of house properties, where you can get to meet up with realtors and chose house of your choice. Built with html and advance grids'
-            link = 'https://kehinded.github.io/FIRST-CSS-GRID-SITE/'
-            github = 'https://github.com/Kehinded/FIRST-CSS-GRID-SITE.git'
-            />
-            
-            <ProjectViewLeft 
-            image = {HotelImg}
-            title = 'Hotel'
-            text = 'Advance online hotel view site for easy online booking . Built with html and css flexbox.'
-            link = 'https://kehinded.github.io/HOTEL/'
-            github = 'https://github.com/Kehinded/HOTEL.git'
-            />
-            <ProjectViewRight 
-            image = {BudgetyImg}
-            title = 'budget app'
-            text = 'a website where you can calculate income and expenses with their data structures. Built this with html, bootstrap and javascript.'
-            link = 'https://kehinded.github.io/Budget-Calculation-app/'
-            github = 'https://github.com/Kehinded/Budget-Calculation-app.git'
-            />
-             <ProjectViewLeft 
-            image = {MeetUp}
-            title = 'Contact save'
-            text = 'online app where you can input, save, remove details or contacts. Built with react .'
-            link = ' https://kehinded.github.io/react-meet-up/'
-            github = 'https://github.com/Kehinded/react-meet-up.git'
-            />
-             <ProjectViewRight 
-            image = {PortfolioImg}
-            title = 'My portfolio'
-            text = 'This current website you are navigating. Built with react and sass with Api actions at the contact and like box, you can get my contacts from the footer.'
-            link = 'https://timi-portfolio.netlify.app/'
-            github = 'https://github.com/Kehinded/Timi-portfolio.git'
-            />
-            <ProjectViewLeft 
-            image = {ResumeImg}
-            title = 'My resume'
-            text = 'My online resume. Built with react and sass.'
-            link = 'https://kehinded.github.io/hngi8-task-cv/'
-            github = 'https://github.com/Kehinded/hngi8-task-cv.git'
-            />
+                
+            {
+             projects.map((child, index) => (
+                 index % 2 === 0 ? (<ProjectViewLeft 
+                 key = {index}
+                 image = {child.image}
+                 title = {child.title}
+                 text = {child.text}
+                 link = {child.link}
+                 github = {child.github}
+                 />) : (<ProjectViewRight 
+                    key = {index}
+                    image = {child.image}
+                    title = {child.title}
+                    text = {child.text}
+                    link = {child.link}
+                    github = {child.github}
+                 />)
+             ))
+         }
             </div>   
         </div>
     )
